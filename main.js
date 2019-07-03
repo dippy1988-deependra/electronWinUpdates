71ffeb5be542c3ffba549b8ad18bf75fb94ae5c4
dippy1988-deependra <dippyd25@gmail.com>
2019-07-03 11:45:18 +0530

    update yml

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 const { app, BrowserWindow } = require('electron')
+const { autoUpdater } = require("electron-updater")
+const log = require('electron-log');
+const isDev = require('electron-is-dev');
+
+
 
 
 let win
+
+
+//Setup Logger
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+//Setup Update Events
+
+autoUpdater.on('checking-for-update', () => {
+ log.info('checking for updates...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  console.log('updates available');
+  console.log('Version', info.version);
+  console.log('release date', info.releaseDate);
+});
+
+autoUpdater.on('update-not-available', () => {
+  console.log('updates not available');
+});
+
+autoUpdater.on('download-progress', (process) => {
+  console.log(`progress ${Math.floor(process.percent)}`);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  console.log('update downloaded');
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('error', () => {
+  console.log('error');
+});
+
+
 
 function createWindow () {
   // Create the browser window.
@@ -27,7 +69,10 @@ function createWindow () {
 }
 
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow
+  autoUpdater.checkForUpdates();
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
