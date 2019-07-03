e03cdf47b5ddcf61dcedf021683fcddd3f3ef45f
dippy1988-deependra <dippyd25@gmail.com>
2019-07-03 11:03:05 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+  
+    win = null
+  })
+}
+
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+ 
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  
+  if (win === null) {
+    createWindow()
+  }
+})
+
